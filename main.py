ac750aa8198e2382501d80d6a1633fb3e0f7fe2a
carlospatinos <carlospatinos@gmail.com>
2022-02-18 17:20:56 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+def heart():
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.HEART)
+basic.forever(heart)
